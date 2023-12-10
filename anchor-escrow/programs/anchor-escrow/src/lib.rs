a6b9e214d967b37f09171cf097f11e6002a7d2ed
jhuhnke <jessica@flipsidecrypto.com>
2023-12-10 17:23:49 -0600

    escrow

---
+++ anchor-escrow/programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,34 @@
+use anchor_lang::prelude::*;
+
+pub mod contexts;
+use contexts::*;
+
+pub mod state;
+pub mod error;
+
+declare_id!("4Hax1x4H4jQjd4xnoutKVDUBbHosoCWVCkKxxMF1STKi");
+
+#[program]
+pub mod anchor_escrow {
+
+    use super::*;
+
+    pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.deposit(deposit)?;
+        ctx.accounts.save_escrow(seed, receive, ctx.bumps.escrow)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw()?;
+        ctx.accounts.close_vault()?;
+        Ok(())
+    }
+
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund()?;
+        ctx.accounts.close_vault()?;
+        Ok(())
+    }
+}
